036eb2f2dbaac7e0d3ee9eaf47451201393f5bd7
Rocco824 <104271637+Rocco824@users.noreply.github.com>
2022-12-19 15:34:55 +0800

    Add files via upload

---
+++ 1219/pdoInc.php
@@ -0,0 +1,16 @@
+﻿﻿<?php
+$db_server = "localhost";
+$db_user = "rocco";
+$db_passwd = "896314sf";
+$db_name = "rocco";
+ 
+try {
+    $dsn = "mysql:host=$db_server;dbname=$db_name";
+    $dbh = new PDO($dsn, $db_user, $db_passwd);
+}
+catch (Exception $e){
+    die('無法對資料庫連線');
+}
+ 
+$dbh->exec("SET NAMES utf8");
+?>
